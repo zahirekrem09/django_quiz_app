548bcc47b77290655d1b5b00e2cc21ae1add843e
zahirekrem09 <1820980144@stu.adu.edu.tr>
2021-01-14 21:39:24 +0200

    added nested admin

--- src/quiz/settings.py
+++ src/quiz/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework', 'quiz_app'
+    'rest_framework',
+    'quiz_app',
+    'nested_admin',
 
 ]
 
